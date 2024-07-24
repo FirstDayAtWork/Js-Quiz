7275b4b57d364bf520cdded105526cdd52a95736
Michael <fatlycka@gmail.com>
2024-07-24 23:52:03 +0300

    fix font path

--- src/scripts/utils/colorCoding.js
+++ src/scripts/utils/colorCoding.js
@@ -2,15 +2,8 @@
 
 export function checkString(str, regexp, name, htmlTxt){
     let match = []
-    // if(!!/\b["`']\b/g.test(str.textContent)){
-    //     str.textContent = str.textContent.replace(/\b["`']\b/g, x => `"`)
-    // }
     if(!!str.textContent.match(regexp)){
         match = [...new Set(str.textContent.match(regexp))]
-        // if(match.some(el => el === "')\"" || el === '"");("' || el === "|| {} || null)const two = (null || false || '")){
-        //     console.log('it is!')
-        //     return
-        // }
         match.map(el => {
             console.log(el)
             let r_part = el
@@ -57,20 +50,6 @@ export function checkString(str, regexp, name, htmlTxt){
                 str.innerHTML = str.innerHTML.trim().replace(r, `<code class=${name}><a href="${el}">${el}</a></code>`)
                 return
             }
-            // let s = '(?<![a-z])'
-            // let e = '(?![a-z])'
-            // s = '(?<!\\/\\/\\s+)'
-            // if(el.includes('$')){
-            //     r_part = el.replace('$', '\\$')
-            // }
-
-            // if(el.includes('//')){
-            //     console.log('we here')
-            //     r_part = '\\/\\/\\s+\\S+'
-            //     s = ''
-            //     e = ''
-            //     console.log(s + r_part + e)
-            // }
             let r = new RegExp(s+r_part+e, 'gi')
             str.innerHTML = str.innerHTML.trim().replace(r, `<code class=${name}>${el}</code>`) 
             console.log('r', r, str.innerHTML)
